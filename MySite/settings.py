f941860d4b398ec2e6a77f754a4fcb580db1ebe9
NURSLORD <normosbekov@gmail.com>
2022-07-10 21:09:54 +0600

    myfirst

--- MySite/settings.py
+++ MySite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 import os
 from pathlib import Path
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -27,7 +28,18 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 # Application definition
+# CORS SETTING
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ORIGIN_ALLOW_ALL = True
+# YOU SHOULD ADD TO MIDDLEWARE
+CORS_ALLOWED_ORIGINS = [
+    "https://example.com",
+    "https://sub.example.com",
+    "http://localhost:3000",
+    "http://127.0.0.1:9000",
+]
 
+# END CORS SETTING
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -36,9 +48,14 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'post.apps.PostConfig',
+    'djoser',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    # CORS MUST BE FIRST
+    'corsheaders.middleware.CorsMiddleware',
+    # END CORS
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -126,3 +143,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+# for heroku
+django_heroku.settings(locals())
